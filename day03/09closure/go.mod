f1bfe0c717ece5acf87dccbc9413ec6253f04225
wangheng <wh9697yyk@163.com>
2022-06-18 23:31:30 +0800

    闭包

---
+++ day03/09closure/go.mod
@@ -0,0 +1,3 @@
+module github.com/wanghengg/09closure
+
+go 1.16
---
+++ day03/09closure/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func adder(x int) func(int) int {
+	return func(y int) int {
+		x += y
+		return x
+	}
+}
+
+func calc(base int) (func(int) int, func(int) int) {
+	add := func(i int) int {
+		base += i
+		return base
+	}
+
+	sub := func(i int) int {
+		base -= i
+		return base
+	}
+	return add, sub
+}
+
+func main() {
+	ret := adder(100)
+	fmt.Println(ret(200))
+
+	f1, f2 := calc(10)
+	fmt.Println(f1(1), f2(2))
+	fmt.Println(f1(3), f2(4))
+	fmt.Println(f1(5), f2(6))
+}
