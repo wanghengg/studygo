b27cdb2fb6336fef102c1def4e6689a15f3d0904
wanghengg <wh9697yyk@163.com>
2022-06-18 11:59:35 +0800

    modified

--- 03day/06func_type/main.go
+++ 03day/06func_type/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func f1() {
 	fmt.Println("Hello, world!")
@@ -10,9 +12,24 @@ func f2() int {
 	return 10
 }
 
+// 函数作为参数
+func f3(x func() int) {
+	ret := x()
+	fmt.Println(ret)
+}
+
+func f5(x func() int) func(int, int) int {
+	ret := func(a, b int) int {
+		return a + b
+	}
+	return ret
+}
+
 func main() {
 	a := f1
 	fmt.Printf("%T\n", a)
 	b := f2
 	fmt.Printf("%T\n", b)
+	f3(f2)
+	fmt.Println(f5(f2)(2, 3))
 }
